4f252fb28357f237aca4babe70757f49a12a7ea1
mzweigert <mateuszzweigert@gmail.com>
2019-08-04 23:43:55 +0200

    Move creating sentences to separate file and write tests.

--- src/main/java/com/mzweigert/sentence_serializer/domain/Sentence.java
+++ src/main/java/com/mzweigert/sentence_serializer/domain/Sentence.java
@@ -1,13 +1,14 @@
 package com.mzweigert.sentence_serializer.domain;
 
 import java.util.Collection;
+import java.util.TreeSet;
 
 public class Sentence {
 
     private Collection<String> words;
 
-    public Sentence(Collection<String> words) {
-        this.words = words;
+    public Sentence() {
+        this.words = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
     }
 
     public boolean addWord(String word) {
--- src/main/java/com/mzweigert/sentence_serializer/service/reader/SentencesReader.java
+++ src/main/java/com/mzweigert/sentence_serializer/service/reader/SentencesReader.java
@@ -8,80 +8,27 @@ import java.util.*;
 public class SentencesReader {
 
     private File file;
-    private List<Sentence> sentences;
-    private Sentence currentSentence;
-    private WordCheckType lastWordCheckType;
-    private AbbreviationsRecognizer recognizer;
 
     public SentencesReader(File file) {
         this.file = file;
-        this.recognizer = new AbbreviationsRecognizer();
-        this.sentences = new LinkedList<>();
-        this.lastWordCheckType = WordCheckType.NOT_CHECK;
-        this.currentSentence = new Sentence(new TreeSet<>(String.CASE_INSENSITIVE_ORDER));
     }
 
     public Collection<Sentence> read() throws LengthyInputException {
         String line;
+        SentenceCreator sentenceCreator = new SentenceCreator();
 
         try(FileReader fileReader = new FileReader(file);
             BufferedReader br = new BufferedReader(fileReader)) {
             while ((line = br.readLine()) != null) {
-                processLine(line);
+                sentenceCreator.processNext(line);
             }
-            return sentences;
+            return sentenceCreator.getSentences();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (OutOfMemoryError e) {
             throw new LengthyInputException("Input is too lengthy.");
         }
         return Collections.emptyList();
-    }
-
-    private void processLine(String line) {
-        String[] words = line.split("[\\s*%&(),\":-]+");
-        for (String word : words) {
-            if(word.equals("")) {
-                continue;
-            }
-
-            if (isEndOfSentenceAfterAbbreviation(lastWordCheckType, word)) {
-                initNewSentence();
-            }
-
-            checkWord(word);
-        }
-    }
-
-    private boolean isEndOfSentenceAfterAbbreviation(WordCheckType wordCheckType, String word) {
-        return wordCheckType == WordCheckType.AFTER_TIME_ABBREVIATION && Character.isUpperCase(word.charAt(0));
-    }
-
-    private void checkWord(String word) {
-        if (isNotEndingWord(word) || recognizer.isPrefixTitleAbbreviation(word)) {
-            lastWordCheckType = WordCheckType.NOT_CHECK;
-            currentSentence.addWord(word);
-        } else if (recognizer.isTimesAbbreviation(word)) {
-            lastWordCheckType = WordCheckType.AFTER_TIME_ABBREVIATION;
-            currentSentence.addWord(word);
-        } else {
-            lastWordCheckType = WordCheckType.END_OF_SENTENCE;
-            word = word.substring(0, word.length() - 1);
-            if(!word.equals("")) {
-                currentSentence.addWord(word);
-            }
-            initNewSentence();
-        }
-    }
-
-    private boolean isNotEndingWord(String word) {
-        char endChar = word.charAt(word.length() - 1);
-        return endChar != '.' && endChar != '?' && endChar != '!';
-    }
-
-    private void initNewSentence() {
-        sentences.add(currentSentence);
-        currentSentence = new Sentence(new TreeSet<>(String.CASE_INSENSITIVE_ORDER));
     }
 
 }
