26b18b99357c9ee028e8e4ff76aac5d74fe30744
mzweigert <mateuszzweigert@gmail.com>
2019-08-04 22:36:42 +0200

    Add main functionality with test

--- src/main/java/com/mzweigert/sentence_serializer/service/reader/SentencesReader.java
+++ src/main/java/com/mzweigert/sentence_serializer/service/reader/SentencesReader.java
@@ -3,21 +3,85 @@ package com.mzweigert.sentence_serializer.service.reader;
 import com.mzweigert.sentence_serializer.domain.Sentence;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SentencesReader {
 
     private File file;
+    private List<Sentence> sentences;
+    private Sentence currentSentence;
+    private WordCheckType lastWordCheckType;
+    private AbbreviationsRecognizer recognizer;
 
     public SentencesReader(File file) {
         this.file = file;
+        this.recognizer = new AbbreviationsRecognizer();
+        this.sentences = new LinkedList<>();
+        this.lastWordCheckType = WordCheckType.NOT_CHECK;
+        this.currentSentence = new Sentence(new TreeSet<>(String.CASE_INSENSITIVE_ORDER));
     }
 
-    public Collection<Sentence> read() {
+    public Collection<Sentence> read() throws LengthyInputException {
+        String line;
 
+        try(FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader)) {
+            while ((line = br.readLine()) != null) {
+                processLine(line);
+            }
+            return sentences;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (OutOfMemoryError e) {
+            throw new LengthyInputException("Input is too lengthy.");
+        }
         return Collections.emptyList();
     }
+
+    private void processLine(String line) {
+        String[] words = line.split("[\\s*%&(),\":-]+");
+        for (String word : words) {
+            if(word.equals("")) {
+                continue;
+            }
+
+            if (isEndOfSentenceAfterAbbreviation(lastWordCheckType, word)) {
+                initNewSentence();
+            }
+
+            checkWord(word);
+        }
+    }
+
+    private boolean isEndOfSentenceAfterAbbreviation(WordCheckType wordCheckType, String word) {
+        return wordCheckType == WordCheckType.AFTER_TIME_ABBREVIATION && Character.isUpperCase(word.charAt(0));
+    }
+
+    private void checkWord(String word) {
+        if (isNotEndingWord(word) || recognizer.isPrefixTitleAbbreviation(word)) {
+            lastWordCheckType = WordCheckType.NOT_CHECK;
+            currentSentence.addWord(word);
+        } else if (recognizer.isTimesAbbreviation(word)) {
+            lastWordCheckType = WordCheckType.AFTER_TIME_ABBREVIATION;
+            currentSentence.addWord(word);
+        } else {
+            lastWordCheckType = WordCheckType.END_OF_SENTENCE;
+            word = word.substring(0, word.length() - 1);
+            if(!word.equals("")) {
+                currentSentence.addWord(word);
+            }
+            initNewSentence();
+        }
+    }
+
+    private boolean isNotEndingWord(String word) {
+        char endChar = word.charAt(word.length() - 1);
+        return endChar != '.' && endChar != '?' && endChar != '!';
+    }
+
+    private void initNewSentence() {
+        sentences.add(currentSentence);
+        currentSentence = new Sentence(new TreeSet<>(String.CASE_INSENSITIVE_ORDER));
+    }
+
 }
---
+++ src/test/java/com/mzweigert/sentence_serializer/service/reader/SentencesReaderTest.java
@@ -0,0 +1,36 @@
+package com.mzweigert.sentence_serializer.service.reader;
+
+import com.mzweigert.sentence_serializer.domain.Sentence;
+import org.junit.Test;
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SentencesReaderTest {
+
+    private File readFile(String name) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL url = Objects.requireNonNull(classLoader.getResource(name));
+        return new File(url.getFile());
+    }
+
+    @Test
+    public void givenFile_whenReadSentences_thenSuccessReadSortedSentences() throws Exception {
+        //GIVEN
+        File file = readFile("small.in");
+
+        //WHEN
+        Collection<Sentence> read = new SentencesReader(file).read();
+
+        //THEN
+        assertThat(read).isNotEmpty();
+        read.forEach(sentence -> {
+            List<String> strings = new ArrayList<>(sentence.getWords());
+            assertThat(strings).isSortedAccordingTo(String.CASE_INSENSITIVE_ORDER) ;
+        });
+    }
+
+
+}
